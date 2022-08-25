ec7bc42def20d00701ad612cfbc24baf4b4fa0bd
Arely Rivera Gonzalez <arelyrivera1996@gmail.com>
2022-08-25 01:45:52 -0700

    added style in css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,8 @@
 //naming variables
+var currentDayEl = $('#currentDay');
+
+var today = moment();
+console.log(today);
+
+currentDayEl.text(today.format("dddd, MMMM Do"));
+console.log(currentDayEl);
