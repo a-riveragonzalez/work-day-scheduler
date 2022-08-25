f134affb7ac3d6d5b9f99352302aceaa5fd5bc0f
Arely Rivera Gonzalez <arelyrivera1996@gmail.com>
2022-08-25 14:01:16 -0700

    added psudo class and todo comments on js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,21 @@
 var currentDayEl = $('#currentDay');
 
 var today = moment();
-console.log(today);
 
 currentDayEl.text(today.format("dddd, MMMM Do"));
-console.log(currentDayEl);
+
+// todo setting up local storage
+// todo set up buttons so they save whats on the input text into local storage
+
+// todo upon refresh get from local storage and show it on input field (text content)
+
+
+
+// todo this function should start on refresh (so call it)
+// todo set up rows so they turn colors based on the time 
+// todo if moment now is === to moment on row, then present
+    //display present. display none past, display non future
+// todo else if it after moment on row , then future
+    //display future. display none past, display non present
+// todo else it is in the past 
+    //display past. display none present, display non future
