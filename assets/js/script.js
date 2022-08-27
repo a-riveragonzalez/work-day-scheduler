96e2a0911d94ae46d11e4993ac3a953ddb559006
Arely Rivera Gonzalez <arelyrivera1996@gmail.com>
2022-08-26 18:22:18 -0700

    added local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,21 @@ var currentDayEl = $("#currentDay");
 var inputEls = $("input");
 console.log(inputEls)
 
+var icon9 = $("#icon9"); //for testing
+
+var iconEls = $(".saveBtn");
+
 var today = moment();
 var todayHour = moment().format("H");
 
-console.log(todayHour)
-
+// on page load, sets time blocks' background colors
 inputEls.each(function(i , input){
-  console.log($(input).attr("id"))
   $(input).addClass(setBGColor($(input).attr("id")))
 
+})
+
+iconEls.each(function(i, icon){
+  $(icon).on("click", setStorage)
 })
 
 function setBGColor(id){
@@ -23,29 +29,34 @@ function setBGColor(id){
     return "future";
   }
 }
-  
-
-
-
-
-// todo this function should start on refresh (so call it)
-// ? set data-state = 9am . So if data-state.value = moment(format(h -- use military time for less confusion))
-// todo set up rows so they turn colors based on the time
-// todo if moment now is === to moment on row, then present
-//display present. display none past, display non future
-
-// todo else if it after moment on row , then future
-//display future. display none past, display non present
-
-// todo else it is in the past
-//display past. display none present, display non future
 
 // todo setting up local storage
 // todo set up buttons so they save whats on the input text into local storage
+function setStorage(event){
+  var input = event.currentTarget.previousElementSibling.firstElementChild.value; //get correct text 
+  var id = event.currentTarget.previousElementSibling.firstElementChild.id;
+  // console.log("do i work?");
+  // console.log(input);
+  localStorage.setItem(id + "-text", input);
+
+}
 
 // todo upon refresh get from local storage and show it on input field (text content)
+function renderStorage(){
+  // can text be saved to input tag ? 
+  // var savedText = localStorage.getItem("9am-text");
+  // inputEls[0].value = savedText;
+  var times = ["09", "10", "11", "12", "13" , "14", "15", "16", "17"];
+  times.forEach(function(timeItems){
+    var savedText = localStorage.getItem(timeItems + "-text");
+    $("#"+timeItems).val(savedText);
+  })
 
+}
 
 //Sets the date
 currentDayEl.text(today.format("dddd, MMMM Do"));
 
+// add event listener
+
+renderStorage();
